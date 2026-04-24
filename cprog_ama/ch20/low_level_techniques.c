0d956b90139aed48373e29cabbd8a9b06ee614ee
Abhishek Pal <98508258+abhiPL07@users.noreply.github.com>
2026-04-25 04:33:22 +0900

    [260425] chapter 20 practice

---
+++ cprog_ama/ch20/low_level_techniques.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+static typedef unsigned char BYTE;
+
+int runLlt(void) {
+    // typedef unsigned char BYTE;
+    // BYTE *p;
+    // p = (BYTE *) 0x1000;
+    // printf("At address %hhn:\t%hx\n", p, *p);
+    return 0;
+}
+
+int viewMemLoc(unsigned int addr, int n) {
+    // int i, n;
+    // BYTE *ptr;
+    // printf("Address of viewMemLoc: %x\n", (unsigned int) viewMemLoc);
+    // printf("Address of addr: %x\n", (unsigned int) &addr);
+    return 0;
+}
--- cprog_ama/ch20/main.c
+++ cprog_ama/ch20/main.c
@@ -1,7 +1,39 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
 
-int main(void) {
-    printf("make success.\n");
-    exit(EXIT_SUCCESS);
+typedef unsigned char BYTE;
+int main(void)
+{
+    uintptr_t addr;
+    int i, n;
+    BYTE *ptr;
+    printf("Address of main function: %" PRIxPTR "\n", (uintptr_t) main);
+    printf("Address of addr variable: %" PRIxPTR "\n", (uintptr_t) &addr);
+    printf("\nEnter a (hex) address: ");
+    scanf("%" SCNxPTR, &addr);
+    printf("Enter number of bytes to view: ");
+    scanf("%d", &n);
+    printf("\n");
+    printf(" Address Bytes Characters\n");
+    printf(" ------- ----------------------------- ----------\n");
+    ptr = (BYTE *) addr;
+    for (; n > 0; n -= 10) {
+        printf("%8" PRIXPTR " ", (uintptr_t) ptr);
+        for (i = 0; i < 10 && i < n; i++)
+            printf("%.2X ", *(ptr + i));
+        for (; i < 10; i++)
+            printf("   ");
+        printf(" ");
+        for (i = 0; i < 10 && i < n; i++) {
+            BYTE ch = *(ptr + i);
+            if (!isprint(ch))
+            ch = '.';
+            printf("%c", ch);
+        }
+        printf("\n");
+        ptr += 10;
+    }
+    return 0;
 }
